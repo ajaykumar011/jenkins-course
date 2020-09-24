dba783733ea920e97dfb04c06cf3018fbf232316
ajay <ajay@cloudzone.today>
2020-09-24 12:24:44 +0000

    first commit

--- Terraform-with-Jenkins/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform{
-    backend "s3"{
-        bucket = "terraform-state-4545"
-        key = "terraform/myproject"
-        region = "ap-south-1"
-    }
-}
---
+++ Terraform-with-Jenkins/main.tf
@@ -0,0 +1,18 @@
+  
+provider "aws" {
+	version = "~> 2.65"
+	region  = var.region
+}
+
+locals {
+	name = "${terraform.workspace}-instance"
+}
+
+resource "aws_instance" "main" {
+	ami           = var.ami
+	instance_type = var.instance_type
+
+	tags = {
+		Name = local.name
+	}
+}
